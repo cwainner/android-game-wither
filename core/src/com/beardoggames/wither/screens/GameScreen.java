9769c64e3a01f26003dcb0034526bcdd15dfd161
Chris Wainner <chris.jwainner@gmail.com>
2017-06-19 08:41:04 -0700

    Add GameScreen with player sprite, update MainMenuScreen to start GameScreen on touch

--- core/src/com/beardoggames/wither/screens/GameScreen.java
+++ core/src/com/beardoggames/wither/screens/GameScreen.java
@@ -4,19 +4,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.beardoggames.wither.Wither;
 
-public class MainMenuScreen implements Screen{
+public class GameScreen implements Screen{
   private final Wither game;
-
   private OrthographicCamera camera;
+  private Rectangle player;
+  private Texture playerSprite;
 
-  public MainMenuScreen(final Wither game){
+  public GameScreen(final Wither game){
     this.game = game;
 
+    // Create Textures
+    playerSprite = new Texture(Gdx.files.internal("badlogic.jpg"));
+
+    // Create the camera
     camera = new OrthographicCamera();
     camera.setToOrtho(false, 800, 480);
+
+    // Create a rectangle to represent the player
+    player = new Rectangle();
+    player.x = 800 / 2 - 64 / 2;
+    player.y = 20;
+    player.width = 64;
+    player.height = 64;
   }
 
   @Override
@@ -30,21 +43,18 @@ public class MainMenuScreen implements Screen{
     Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-    // Update camera and set game batch to use its coordinate system
+    // Update the camera
     camera.update();
+
+    // Tell the SpriteBatch to render in the coord system used by the camera
     game.batch.setProjectionMatrix(camera.combined);
 
-    // Begin a new batch to draw
+    // Begin a new batch
     game.batch.begin();
-    game.font.draw(game.batch, "Welcome to Wither!", 800 / 2, 480 / 2);
-    game.font.draw(game.batch, "Tap anywhere to begin", 800 / 2, 480 / 2 - 50);
+    game.batch.draw(playerSprite, player.x, player.y, player.width, player.height);
     game.batch.end();
 
     // Process user input
-    if(Gdx.input.isTouched()){
-      game.setScreen(new GameScreen(game));
-      dispose();
-    }
   }
 
   @Override
