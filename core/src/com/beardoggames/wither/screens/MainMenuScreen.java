5bb160bbb3ffbe5acdbbb604066fa8dc1aec0654
Chris Wainner <chris.jwainner@gmail.com>
2017-06-19 08:23:29 -0700

    Add Welcome message to MainMenuScreen, add basic Screen and render setup

--- core/src/com/beardoggames/wither/screens/MainMenuScreen.java
+++ core/src/com/beardoggames/wither/screens/MainMenuScreen.java
@@ -1,13 +1,21 @@
 package com.beardoggames.wither.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.beardoggames.wither.Wither;
 
 public class MainMenuScreen implements Screen{
   private final Wither game;
 
+  private OrthographicCamera camera;
+
   public MainMenuScreen(final Wither game){
     this.game = game;
+
+    camera = new OrthographicCamera();
+    camera.setToOrtho(false, 800, 480);
   }
 
   @Override
@@ -17,7 +25,16 @@ public class MainMenuScreen implements Screen{
 
   @Override
   public void render(float delta) {
+    Gdx.gl.glClearColor(0, 0, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+    camera.update();
+    game.batch.setProjectionMatrix(camera.combined);
+
+    game.batch.begin();
+    game.font.draw(game.batch, "Welcome to Wither!", 800 / 2, 480 / 2);
+    game.font.draw(game.batch, "Tap anywhere to begin", 800 / 2, 480 / 2 - 50);
+    game.batch.end();
   }
 
   @Override
