f56a7263f9fcfa4548adf3484a5029a49cc2bb0f
Chris Wainner <chris.jwainner@gmail.com>
2017-06-19 08:13:57 -0700

    Refactor App to be a game object, add MainMenuScreen

---
+++ core/src/com/beardoggames/wither/screens/MainMenuScreen.java
@@ -0,0 +1,47 @@
+package com.beardoggames.wither.screens;
+
+import com.badlogic.gdx.Screen;
+import com.beardoggames.wither.Wither;
+
+public class MainMenuScreen implements Screen{
+  private final Wither game;
+
+  public MainMenuScreen(final Wither game){
+    this.game = game;
+  }
+
+  @Override
+  public void show() {
+
+  }
+
+  @Override
+  public void render(float delta) {
+
+  }
+
+  @Override
+  public void resize(int width, int height) {
+
+  }
+
+  @Override
+  public void pause() {
+
+  }
+
+  @Override
+  public void resume() {
+
+  }
+
+  @Override
+  public void hide() {
+
+  }
+
+  @Override
+  public void dispose() {
+
+  }
+}
--- desktop/src/com/beardoggames/wither/desktop/DesktopLauncher.java
+++ desktop/src/com/beardoggames/wither/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.beardoggames.wither.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.beardoggames.wither.App;
+import com.beardoggames.wither.Wither;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new App(), config);
+		new LwjglApplication(new Wither(), config);
 	}
 }
