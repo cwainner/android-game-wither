4460493a59c5a0f535e817eed80a5a0f3cf7fa66
cwainner <brennenburg@gmail.com>
2017-06-20 15:18:14 -0700

    Add fixture user data to player and ground

--- core/src/com/beardoggames/wither/models/Player.java
+++ core/src/com/beardoggames/wither/models/Player.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
@@ -43,7 +44,8 @@ public class Player extends Sprite{
     fixtureDef.shape = shape;
     fixtureDef.density = 1f;
 
-    body.createFixture(fixtureDef);
+    Fixture fixture = body.createFixture(fixtureDef);
+    fixture.setUserData("player");
 
     shape.dispose();
   }
